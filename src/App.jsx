76eda45d1b23862e943ff82d23c8221408ba3438
Satyamrk <satyamrk18@gmail.com>
2025-08-23 21:22:16 +0530

    sample qr code added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import './App.css'
-
+import "./App.css";
+import { useState, useEffect } from "react";
+import { QRCodeCanvas } from "qrcode.react";
 function App() {
-  return(
+  const [text, setText] = useState("");
+  return (
     <div>
       <h1>Hello world</h1>
+      <form>
+        <label>input</label>
+        <input
+          type="text"
+          name="qir input"
+          placeholder="ie, text, url"
+          value={text}
+          onChange={(e) => {
+            setText(e.target.value);
+          }}
+        />
+      </form>
+      <QRCodeCanvas
+        value={text}
+        size={200}
+        bgColor="#f0f8ff"
+        fgColor="#1e3a8a"
+      ></QRCodeCanvas>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
