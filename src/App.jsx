4fcc27ab88a02e5a9a961a16c4f64221eff5821d
Satyamrk <satyamrk18@gmail.com>
2025-08-24 14:27:51 +0530

    range based qr code size changed

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,44 @@ import { useState, useEffect } from "react";
 import { QRCodeCanvas } from "qrcode.react";
 function App() {
   const [text, setText] = useState("");
+  const [size, setSize] = useState(100);
   return (
     <div className="container">
- <div className="text-input">
+      <div className="text-input">
         <form>
-        <label>input</label>
-        <input
-          type="text"
-          name="qir input"
-          placeholder="ie, text, url"
-          value={text}
-          onChange={(e) => {
-            setText(e.target.value);
-          }}
-        />
-      </form>
-    </div>
+          <label>input</label>
+          {/* input */}
+          <input
+            type="text"
+            name="qir input"
+            placeholder="ie, text, url"
+            value={text}
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+          />
+          {/* size */}
+          <input
+            type="range"
+            min="10"
+            max="100"
+            value={size}
+            onChange={(e) => {
+              setSize(e.target.value);
+            }}
+          /><p>{size}</p>
+        </form>
+      </div>
       {/* qr code canvas */}
-     <div className="qr-canvas">
-       <QRCodeCanvas
-        value={text}
-        size={220}
-        bgColor="#ffffffff"
-        fgColor="#1e3b8aa8"
-        level="H" 
-      ></QRCodeCanvas>
-     </div>
+      <div className="qr-canvas">
+        <QRCodeCanvas
+          value={text}
+          size={size*5}
+          bgColor="#ffffffff"
+          fgColor="#1e3b8aa8"
+          level="H"
+        ></QRCodeCanvas>
+      </div>
     </div>
   );
 }
