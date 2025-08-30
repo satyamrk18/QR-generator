9f97f33bb42e8839b85e6b00ffe8e3554b683a73
Satyamrk <satyamrk18@gmail.com>
2025-08-30 11:48:38 +0530

    paymnet form added

--- src/pages/paymnet.jsx
+++ src/pages/paymnet.jsx
@@ -1,11 +1,31 @@
-import Navbar from "../components/navbar.jsx"
-import "./payment.css"
-const payment = ()=>
-{
-    return(
+import Navbar from "../components/navbar.jsx";
+import { useState, useEffect, use } from "react";
+import "./payment.css";
+const payment = () => {
+  const [payeeName, setPayeeName] = useState("");
+  const [upiID, setUpiID] = useState("");
+  const [amount, setAmount] = useState(0);
+  const [note, setNote] = useState("");
+  const [currency, setCurrency] = useState("INR");
+  return (
     <div className="payments">
-        <div><Navbar /></div>
-        <div><h1>payment page</h1></div>
-    </div>);
-}
+      <div>
+        <Navbar />
+      </div>
+      <div className="main-container">
+        <div className="inputs">
+            <label>payee name</label>
+          <input type="text" placeholder="payee name"/>
+          <label>upi id</label>
+          <input type="text" placeholder="upi id"/>
+          <label>amount</label>
+          <input type="number" placeholder="amount"/>
+          <label>note</label>
+          <input type="text" placeholder="note"/>
+        </div>
+        <div className="qr-canvas"></div>
+      </div>
+    </div>
+  );
+};
 export default payment;
