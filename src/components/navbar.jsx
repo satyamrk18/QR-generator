46d0907da54d5de0635e49b3970c49f26d094540
Satyamrk <satyamrk18@gmail.com>
2025-08-30 19:09:12 +0530

    page based styling added to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,12 @@
-import { Link, useLocation } from "react-router";
+import { Link, useLoaderData, useLocation } from "react-router";
 import "./navbar.css"
 const Navbar = () => {
+  const location  = useLocation();
   return (
     <div className="navbar">
-      <Link to="/" className="item">Home</Link>
-      <Link to="/payment" className="item">Payment</Link>
-      <Link to= "/map" className="item">Map</Link>
+      <Link to="/" className={location.pathname == "/"? "activepage" : "item"}>Home</Link>
+      <Link to="/payment" className={location.pathname=="/payment" ? "activepage" : "item"}>Payment</Link>
+      <Link to= "/map" className={location.pathname=="/map"?"activepage":"item"}>Map</Link>
     </div>
   );
 };
