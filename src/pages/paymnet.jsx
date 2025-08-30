5cc770f2826045d4ebb6ae756f693d447cb3e6a6
Satyamrk <satyamrk18@gmail.com>
2025-08-30 18:28:47 +0530

    paymnet integration added for all types of mayment and testing suxxessfull

--- src/pages/paymnet.jsx
+++ src/pages/paymnet.jsx
@@ -4,6 +4,7 @@ import { QRCodeCanvas } from "qrcode.react";
 import Download from "../downloadBtn.jsx";
 import "./payment.css";
 const payment = () => {
+  const [paymentURL, setPaymnetURL] = useState("");
   const [payeeName, setPayeeName] = useState("");
   const [upiID, setUpiID] = useState("");
   const [amount, setAmount] = useState(0);
@@ -11,19 +12,18 @@ const payment = () => {
   const [currency, setCurrency] = useState("INR");
   const [qrValue, setQrValue] = useState("");
   const [size, setSize] = useState(50);
-const[color, setColor] = useState("#000000");
+  const [color, setColor] = useState("#000000");
   //initial qr value
   useEffect(() => {
     setQrValue("https://github.com/satyamrk18");
   }, []);
+  useEffect(() => {
+    setPaymnetURL(`upi://pay?pa=${upiID}&pn=${payeeName}&am=${amount}&tn=${note}&cu=${currency}
+`);
+  }, [payeeName, upiID, amount, note]);
 
   const handleGenerateQR = () => {
-    if (inputText.trim() !== "") {
-      setQrValue(inputText);
-    }
-    if (inputText.trim() === "") {
-      alert("please enter the text, link or anything !");
-    }
+    setQrValue(paymentURL);
   };
   //download button referace
   const captureRef = useRef(null);
@@ -34,14 +34,39 @@ const[color, setColor] = useState("#000000");
       </div>
       <div className="main-container">
         <div className="inputs">
+          <h1>payment qr</h1>
           <label>payee name</label>
-          <input type="text" placeholder="payee name" />
+          <input
+            type="text"
+            placeholder="payee name"
+            onChange={(e) => {
+              setPayeeName(e.target.value);
+            }}
+          />
           <label>upi id</label>
-          <input type="text" placeholder="upi id" />
+          <input
+            type="text"
+            placeholder="upi id"
+            onChange={(e) => {
+              setUpiID(e.target.value);
+            }}
+          />
           <label>amount</label>
-          <input type="number" placeholder="amount" />
+          <input
+            type="number"
+            placeholder="amount"
+            onChange={(e) => {
+              setAmount(e.target.value);
+            }}
+          />
           <label>note</label>
-          <input type="text" placeholder="note" />
+          <input
+            type="text"
+            placeholder="note"
+            onChange={(e) => {
+              setNote(e.target.value);
+            }}
+          />
           <label>size</label>
           <input
             type="range"
@@ -49,7 +74,9 @@ const[color, setColor] = useState("#000000");
             min="10"
             max="100"
           />
-          <button type="button" onClick={handleGenerateQR}></button>
+          <button type="button" onClick={handleGenerateQR}>
+            Generate QR
+          </button>
         </div>
         <div className="qr-canvas" ref={captureRef}>
           <div className="qr-code">
