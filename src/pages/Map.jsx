a2bbdaebd3773e1e1a9c6c19e32ff91bc3042915
Satyamrk <satyamrk18@gmail.com>
2025-08-30 18:46:25 +0530

    map page created

---
+++ src/pages/Map.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/navbar"
+import "./Map.css"
+const Map = ()=>
+{
+    return(
+        <div>
+            <div>
+                <Navbar />
+            </div>
+            <h1>Map</h1>
+        </div>
+    );
+}
+export default Map;
