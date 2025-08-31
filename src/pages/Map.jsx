d22b8da9e61e068fe087df9fe5f5bd5221ffe549
Satyamrk <satyamrk18@gmail.com>
2025-08-31 17:24:51 +0530

    location qr added, when user seach a location then it generate a location qr

--- src/pages/Map.jsx
+++ src/pages/Map.jsx
@@ -1,14 +1,39 @@
-import Navbar from "../components/navbar"
-import "./Map.css"
-const Map = ()=>
-{
-    return(
-        <div>
-            <div>
-                <Navbar />
-            </div>
-            <h1>Map</h1>
-        </div>
-    );
-}
+import Navbar from "../components/navbar";
+import "./Map.css";
+import { QRCodeCanvas } from "qrcode.react";
+import { useState,useEffect,useRef } from "react";
+const Map = () => {
+    const [location, setLocation] = useState("");
+    const [qrValue, setqrValue] = useState("");
+
+    const handlegenrateqr = ()=>
+    {
+        setLocation(location)
+        const URL = `https://www.google.com/maps/search/?api=1&query=${location}`;
+        setqrValue(URL)
+    }
+
+  return (
+    <div className="container">
+      <div>
+        <Navbar />
+      </div>
+      <div className="main-container">
+        <h1>Map</h1>
+        <input type="text" onChange={(e)=>{setLocation(e.target.value)}}/>
+<button type="button" onClick={handlegenrateqr}>generate qr</button>
+        <div className="qr-code">
+            {qrValue && (
+              <QRCodeCanvas
+                value={qrValue}
+                // size={size * 5}
+                // fgColor={color}
+                level="H"
+              />
+            )}
+          </div>
+      </div>
+    </div>
+  );
+};
 export default Map;
