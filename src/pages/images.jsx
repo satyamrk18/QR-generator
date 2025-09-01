95d41eafcc3f1e21cc36cfc3658fb70e4d1a1262
Satyamrk <satyamrk18@gmail.com>
2025-09-01 14:27:29 +0530

    seperate image page created

---
+++ src/pages/images.jsx
@@ -0,0 +1,14 @@
+import { useState, useEffect, useRef } from "react";
+import "./images.css"
+import { QRCodeCanvas } from "qrcode.react";
+import Download from "../downloadBtn.jsx"
+import Navbar from "../components/navbar.jsx"
+const image = ()=>
+{
+    return(
+        <div>
+<div><Navbar /></div>
+        </div>
+    )
+}
+export default image;
